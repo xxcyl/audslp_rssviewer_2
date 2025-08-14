34d61c26e52e35d42da6800999723eb47a4efdd2
xxcyl <wittyxx@gmail.com>
2025-08-14 09:12:56 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,7 +62,7 @@ export const metadata: Metadata = {
     },
   },
   verification: {
-    google: "your-google-verification-code",
+    google: process.env.NEXT_PUBLIC_GOOGLE_VERIFICATION,
     // bing: "your-bing-verification-code",
   },
   category: "醫學研究",
